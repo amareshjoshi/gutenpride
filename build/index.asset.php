ea9cbaedbf65dc3b741425f782c65d0cb52baf17
Amaresh R Joshi <joshia@msu.edu>
2022-07-09 22:41:19 -0400

    After running "npm run start".

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '10d04571cd9f8dff3eed');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'cb58b3db1a9c0e03227b');
